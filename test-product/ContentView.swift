3f7cdd12ac7c699e9d419746ddb53bec9af72828
Jacob M Sock <jacobmsock@gmail.com>
2023-01-31 14:53:30 -0500

    Initial Commit

---
+++ test-product/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test-product
+//
+//  Created by Jacob Sock on 1/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test-product/test_productApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_productApp.swift
+//  test-product
+//
+//  Created by Jacob Sock on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct test_productApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
